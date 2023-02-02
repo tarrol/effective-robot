a997c413223dc09c2842cd3fcde54e7336825e14
Jishii <33364545+smudge121@users.noreply.github.com>
2023-02-02 10:25:55 -0800

    front end query plus profile mutation

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -9,3 +9,29 @@ export const QUERY_ME = gql`
     }
   }
 `;
+
+export const QUERY_LIST = gql`
+  query list($_id: String) {
+    list(_id: $_id) {
+      _id
+      name
+      chores {
+        _id
+        name
+        description
+        points
+      }
+    }
+  }
+`;
+
+export const QUERY_REWARD = gql`
+  query reward($_id: String) {
+    reward(_id: $_id) {
+      _id
+      name
+      cost
+    }
+  }
+`;
+
