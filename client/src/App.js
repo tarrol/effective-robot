b4fdb605c564124d59d908349d04e55326b9c9d1
omarm <omarmousa@outlook.com>
2023-02-03 14:48:39 -0800

    Implemented the Front end and back end of the Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,9 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("auth_token");
+
+  console.log(token);
+  
   return {
     headers: {
       ...headers,
